90344543691cb602d4d617c046e210d2017049d2
Jotish <jotishk649@gmail.com>
2025-12-10 14:02:24 +0530

    working

---
+++ header.php
@@ -0,0 +1,166 @@
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-primary text-white d-none d-lg-flex wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-3">
+            <div class="d-flex gap-2 align-items-center">
+                <a href="index.html">
+                    <img src="./img/logo.webp" height="100px" width="100%" class="text-white fw-bold m-0" alt="">
+                </a>
+                <a href="index.html">
+                    <h2 class="text-white fw-bold m-0"> CHAWLA TEXTILES</h2>
+                </a>
+
+                <div class="ms-auto d-flex align-items-center">
+                    <small class="ms-4"><i class="fa fa-map-marker-alt me-3"></i>Khasra Number 1535 Janak Garden
+                        <br>Purewal Colony, Panipat-132103, Haryana, India</small>
+                    <small class="ms-4"><i class="fa fa-envelope me-3"></i>info@example.com</small>
+                    <small class="ms-4"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</small>
+                    <div class="ms-3 d-flex">
+                        <a class="btn btn-sm-square btn-light text-primary ms-2" href=""><i
+                                class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-sm-square btn-light text-primary ms-2" href=""><i
+                                class="fab fa-twitter"></i></a>
+                        <a class="btn btn-sm-square btn-light text-primary ms-2" href=""><i
+                                class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+    <!-- Navbar Start -->
+    <div class="container-fluid bg-white sticky-top wow fadeIn" data-wow-delay="0.1s">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light p-lg-0">
+
+                <!-- MOBILE LOGO -->
+                <a href="index.html" class="navbar-brand d-lg-none">
+                    <img src="./img/logo.webp" height="100" width="100" alt="">
+                </a>
+                <a href="index.html" class="navbar-brand d-none d-lg-none d-sm-block">
+                    <h1 class="fw-bold m-0">CHAWLA TEXTILES</h1>
+                </a>
+
+                <!-- TOGGLE -->
+                <button class="navbar-toggler me-0" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+
+                    <div class="navbar-nav">
+
+                        <a href="index.html" class="nav-item nav-link active">Home</a>
+                        <a href="about.html" class="nav-item nav-link">About</a>
+
+                        <!-- PRODUCTS MULTI LEVEL MENU -->
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Products</a>
+
+                            <ul class="dropdown-menu bg-white border-0">
+
+                                <!-- Cushion Covers -->
+                                <li class="dropdown-submenu">
+                                    <a class="dropdown-item dropdown-toggle" href="#">Cushion Covers</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="Off-White-Tufted-Cushion-Cover.html">Off
+                                                White
+                                                Tufted Cushion Cover </a></li>
+                                        <li><a class="dropdown-item" href="Cotton-Cushion-Cover.html">Cotton Cushion
+                                                Cover</a></li>
+                                        <li><a class="dropdown-item" href="Printed-Velvet-Cushion-Cover.html">Printed
+                                                Velvet Cushion Cover </a></li>
+                                        <li><a class="dropdown-item" href="Polyester-Woven-Cushion-Cover.html">Polyester
+                                                Woven Cushion Cover </a></li>
+                                    </ul>
+                                </li>
+
+                                <!-- Floor Rugs -->
+                                <li class="dropdown-submenu">
+                                    <a class="dropdown-item dropdown-toggle" href="#">Floor Rugs</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="Heart_Shape_Cotton_Rug.html">Heart Shape
+                                                Cotton Rug </a></li>
+                                        <li><a class="dropdown-item" href="Off_White_Polyester_Rug.html">Off White
+                                                Polyester Rug </a></li>
+                                        <li><a class="dropdown-item" href="Washable_Printed_Rugs.html">Washable Printed
+                                                Rugs </a></li>
+                                        <li><a class="dropdown-item" href="Digital_Printed_Cotton_Rugs.html">Digital
+                                                Printed Cotton Rugs </a></li>
+                                    </ul>
+                                </li>
+
+                                <!-- Table Mats -->
+                                <li class="dropdown-submenu">
+                                    <a class="dropdown-item dropdown-toggle" href="#">Table Mats</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="Cotton_Table_Mat.html">Cotton Table Mat</a>
+                                        </li>
+                                        <li><a class="dropdown-item" href="Printed_Mat.html">Printed Mat </a></li>
+                                    </ul>
+                                </li>
+
+                                <!-- Floor Mat -->
+                                <li class="dropdown-submenu">
+                                    <a class="dropdown-item dropdown-toggle" href="#">Floor Mat</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="Bathroom_Rug_Mat.html">Bathroom Rug Mat</a>
+                                        </li>
+                                        <li><a class="dropdown-item" href="Floral_Print_Door_Mat.html">Floral Print Door
+                                                Mat </a></li>
+                                        <li><a class="dropdown-item" href="Pink_Cotton_Door_Mat.html">Pink Cotton Door
+                                                Mat </a></li>
+                                        <li><a class="dropdown-item" href="Tufted_Bath_Mat.html">Tufted Bath Mat</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <!-- Cotton Cushion -->
+                                <li class="dropdown-submenu">
+                                    <a class="dropdown-item dropdown-toggle" href="#">Cotton Cushion</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="Striped_Cotton_Cushion.html">Striped Cotton
+                                                Cushion </a></li>
+                                        <li><a class="dropdown-item" href="Cotton_Floor_Cushion.html">Cotton Floor
+                                                Cushion </a></li>
+                                        <li><a class="dropdown-item" href="Cushion_Floor_Mat.html">Cushion Floor Mat
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <!-- Home Decor -->
+                                <li class="dropdown-submenu">
+                                    <a class="dropdown-item dropdown-toggle" href="#">Home Decor</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="Pouf.html">Pouf</a></li>
+                                        <li><a class="dropdown-item" href="sofa_throw.html">Sofa Throw </a>
+                                        </li>
+                                        <li><a class="dropdown-item" href="Wall_decore.html">Wall Decor</a>
+                                        <li><a class="dropdown-item" href="Wall_hanging.html">Wall Hanging</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                            </ul>
+                        </div>
+
+                        <a href="gallery.html" class="nav-item nav-link">Gallery</a>
+                        <a href="blog.html" class="nav-item nav-link">Blog</a>
+                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+
+                    </div>
+
+                    <div class="ms-auto d-none d-lg-block">
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#enquiryModal"
+                            class="btn btn-primary py-2 px-3">
+                            Enquiry
+                        </a>
+                    </div>
+
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
+     
